b61ad5a56089fadaa233cdaed575d3389f754f2b
marvinorias <oriasmarvinm@gmail.com>
2022-08-16 15:16:44 +0800

    I've added a notif for the user's credentials at the welcome page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Swal from 'sweetalert2';
 
 export default function Home(){
+	useEffect(() => {
+		Swal.fire({
+			text: `The api that I used for the users was mine. I've created an account for codedisruptors.
+			Email: codedisruptors@email.com
+			pw: codedisruptors
+			Thank you and have a nice day!`
+		})
+	}, []);
+
 	return(
 		<>
 			<div className="home">
